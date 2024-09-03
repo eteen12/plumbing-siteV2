b44f304b9aabb877e9d49db680b3048cbc7053ad
eteen <eteensmash@gmail.com>
2024-09-02 18:21:59 -0700

    nav bar styling complete

--- frontend/src/homePage.js
+++ frontend/src/homePage.js
@@ -19,76 +19,85 @@ function HomePage() {
           backgroundRepeat: "no-repeat",
         }}
       >
-
-      <nav className="nav-wrapper">
-        <div className="nav-content-wrapper">
-          <div className="logo-wrapper">
-            <img
-              src="/images/plumbing-logo.png"
-              alt="Your Logo"
-              className="logo"
-            />
+        <nav className="nav-wrapper">
+          <div className="small-screens">
+            <div className="nav-content-wrapper">
+              <div className="logo-wrapper">
+                <img
+                  src="/images/plumbing-logo.png"
+                  alt="Your Logo"
+                  className="logo"
+                />
+              </div>
+              <div className="menu-wrapper" onClick={toggleMenu}>
+                <img src="/images/menu-new.png" className="menu" alt="menu" />
+              </div>
+            </div>
+            <div className={`slide-in-menu ${isMenuOpen ? "open" : ""}`}>
+              <div className="close-btn-wrapper">
+                <button className="close-btn" onClick={toggleMenu}>
+                  <img
+                    src="/images/close.png"
+                    alt="close"
+                    className="close-btn"
+                  />
+                </button>
+              </div>
+              <ul>
+                <li>
+                  <a href="/home">Home</a>
+                </li>
+                <li>
+                  <a href="/services">Services</a>
+                </li>
+                <li>
+                  <a href="/about">About</a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="menu-wrapper" onClick={toggleMenu}>
-            <img src="/images/menu-new.png" className="menu" alt="menu" />
-          </div>
-        </div>
-        <div className={`slide-in-menu ${isMenuOpen ? "open" : ""}`}>
-          <div className="close-btn-wrapper">
-            <button className="close-btn" onClick={toggleMenu}>
-              <img src="/images/close.png" alt="close" className="close-btn" />
-            </button>
-          </div>
-          <ul>
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-              <a href="/services">Services</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-        {/* STYLING FOR BIGGER SCREENS!!!!!!! */}
-        <nav className="nav-bar-large">
-          <div className="logo-wrapper">
-            <img src="/images/plumbing-logo.png" alt="Logo" className="logo" />
-          </div>
-          <div className="menu-wrapper">
-            <ul>
-              <li>
-                <a href="/home" className="page">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/services" className="page">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="/about" className="page">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="/contact" className="page">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+          {/* STYLING FOR BIGGER SCREENS!!!!!!! */}
+          <nav className="large-screens">
+            <div className="logo-wrapper">
+              <img
+                src="/images/plumbing-logo.png"
+                alt="Logo"
+                className="logo-large"
+              />
+            </div>
+            <div className="menu-wrapper">
+              <ul>
+                <li>
+                  <a href="/home" className="page">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="/services" className="page">
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a href="/about" className="page">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a href="/contact" className="page">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </nav>
-      </nav>
-      <div className="text-content-wrapper">
-        <h1 className="header">PLUMBING & PLUMBING CO</h1>
+        <div className="text-content-wrapper">
+          <h1 className="header">PLUMBING & PLUMBING CO</h1>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
